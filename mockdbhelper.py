b975d5674ea9f87a556a9b338e77a6d4ffd82963
Ryan Palo <ryan@thepalos.com>
2016-04-26 19:04:20 -0700

    Corrected syntax error in mockdbhelper

--- mockdbhelper.py
+++ mockdbhelper.py
@@ -18,6 +18,6 @@ class MockDBHelper:
     def get_all_crimes(self):
         return [{'latitude': -33.301304,
                     'longitude': 26.523355,
-                    'date': "2000-01-01,
+                    'date': "2000-01-01",
                     'category': "mugging",
                     'description': "mock description"}]
